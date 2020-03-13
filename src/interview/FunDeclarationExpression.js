8361cfc3c345adab747c112e8c421f2b02f8df2a
Naveen2379 <naveen2379@gmail.com>
2020-03-13 17:54:59 +0530

    JS interview preparation

--- src/interview/FunDeclarationExpression.js
+++ src/interview/FunDeclarationExpression.js
@@ -1,5 +1,5 @@
-console.log(funD());  //function is available before its declaration
-console.log(funE());   //undefined - because function is assigned to a variable here and it is got hoisted  -Cannot access 'funE' before initialization
+//funD();  //function is available before its declaration
+//funE();   //Cannot access 'funE' before initialization - because function is assigned to a variable here and it behaves like a variable
 function funD() {
     console.log('function declaration');
 }
@@ -7,3 +7,9 @@ function funD() {
 let funE = function () {
 console.log('function expression');
 };
+
+/*console.log(b); //Cannot access 'b' before initialization
+let b=10;*/
+
+let a;
+console.log(a); //undefined
